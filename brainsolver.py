a1ceaaad13c01fa3fb90f0179e0c091b544d89de
Ron4Axc1H3g <sdimetrecu@gmail.com>
2022-06-19 11:53:57 -0400

    Add files via upload
    --incomming update support program

---
+++ brainsolver.py
@@ -0,0 +1,180 @@
+#!/usr/bin/python3
+# from codecs import namereplace_errors
+import sys
+# from sympy import init_session
+import sympy as sym
+# from __future__ import division
+from sympy import *
+
+# how to import  library code exmple redirect out in file dump  file 
+# library  context  blocks  >> stdout in file or alternate 
+from contextlib import redirect_stdout
+# import sympy
+from sympy.abc import *
+from sympy.parsing.sympy_parser import *
+import math
+import time
+
+# init_session()
+
+
+def banner():
+    syym ="""
+        '#################################'
+        ' ___ _   _ _ __ ___  _ __  _   _ '
+        '/ __| | | | '_ ` _ \| '_ \| | | |'
+        '\__ \ |_| | | | | | | |_) | |_| |'
+        '|___/\__, |_| |_| |_| .__/ \__, |'
+        '     __/ |          | |     __/ |'
+        '    |___/           |_|    |___/ '
+        ##################################
+    """
+
+    src = """
+    
+     _       __     __    ______               __    ______      __
+    | |     / /__  / /_  / ____/_______  _____/ /_  / ____/___ _/ /___  __  __
+    | | /| / / _ \/ __ \/ /_  / ___/ _ \/ ___/ __ \/ /   / __ `/ / __ \/ / / /
+    | |/ |/ /  __/ /_/ / __/ / /  /  __(__  ) / / / /___/ /_/ / / /_/ / /_/ /
+    |__/|__/\___/_.___/_/   /_/   \___/____/_/ /_/\____/\__,_/_/ .___/\__, /
+                                                              /_/    /____/
+    """
+
+
+    # time.sleep()
+    # for again in ran
+    print(src)
+    # return syym
+
+def diferentials(reguet_integral) -> str:
+    x, y, z = sym.symbols('x y z')
+    w , t , u = sym.symbols('w, t ,u')
+    k, m , n = sym.symbols('k m n' ,integer=True)
+    f, g , h = sym.symbols('f, g, h')
+    sym.init_printing(True)
+    sensitive = "##########################################"
+    complement = pprint(str(sym.Integral(reguet_integral)), use_unicode=False)
+    #complement =  pprint(sym.Integral(reguet_integral), use_unicode=True)
+    print(f'{sensitive}')
+
+    for u in range(0, 1):
+        time.sleep(0.2)                
+        complement = pprint(sym.integrate(reguet_integral), use_unicode=True)
+        with open('fixhtml/output.txt', 'w') as htf:
+            htf.write('\n')
+            with redirect_stdout(htf):
+                complement = pprint(str(sym.Integral(reguet_integral)), use_unicode=False)
+                # htf.write(print('\n'))
+                complement = (print('\n'))
+                complement =  pprint(sym.Integral(reguet_integral))
+                complement = (print('\n'))
+                complement = pprint(sym.integrate(reguet_integral), use_unicode=False)
+                # rint(complement)
+                # htf.write()
+           
+        # pprint(complement)
+        #ad = sym.integrate(reguet_integral), use_unicode=True
+    # how to calculte  doit() operation
+    # complement code insane version out single code ignoring out sample test 
+    # complement.doit()
+    # how instruction resolv integrate  in  machine 
+
+def diclivs(reguet_declivs):
+    x, y, z = sym.symbols('x y z')
+    w , t , u = sym.symbols('w, t ,u')
+    k, m , n = sym.symbols('k m n' ,integer=True)
+    f, g , h = sym.symbols('f, g, h')
+    sym.init_printing(True)
+    reincibilization = "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+    complement = pprint(str(sym.Derivative(reguet_declivs)), use_unicode=False)
+    complement =pprint(sym.Derivative(reguet_declivs), use_unicode=True)
+    for a in range(0, 1):
+        time.sleep(0.1)
+        complement=pprint(sym.diff(reguet_declivs), use_unicode=True)
+        sys.exit(0)
+    # complement.doit()
+def limitation(reguet_limon, reguet_val, reguet_fusse):
+    x, y, z = sym.symbols('x, y, z')
+    w , t , u = sym.symbols('w, t ,u')
+    k, m , n = sym.symbols('k m n' ,integer=True)
+    f, g , h = sym.symbols('f, g, h')
+    sym.init_printing(True)
+    reems = "@##@#@#@#@#@#@#@#@#@#@#@#@@@#@##@##@#@#@#@#@#@"
+    ument = pprint(str(sym.Limit(reguet_limon , reguet_val, reguet_fusse)), use_unicode=False)
+    ument = pprint(sym.Limit(reguet_limon, reguet_val, reguet_fusse), use_unicode=True)
+    print(f'{reems}')
+    for s in range(0, 1):
+        time.sleep(0.1)
+        ument = pprint(sym.limit(reguet_limon, reguet_val, reguet_fusse), use_unicode=True)
+        sys.exit(0)
+    #complement =pprint(sym.Integrate(reguet_limited), use_unicode=False)
+    # complement.doit()
+
+def  eleven():
+    x, y, z = sym.symbols('x y z')
+    w , t , u = sym.symbols('w, t ,u')
+    k, m , n = sym.symbols('k m n' ,integer=True)
+    f, g , h = sym.symbols('f, g, h')
+    sym.init_printing(True)
+    if  sys.argv[1] == "--mode":
+        if sys.argv[2] == "--int":
+            sensitive = "#############################################"
+            complet = pprint(str(sym.Integral(sys.argv[3])), use_unicode=True)
+            complet = pprint(sym.Integral(sys.argv[3]), use_unicode=False)
+            print(f'{sensitive}')
+            for u in range(0, 1):
+                time.sleep(0.2)                
+                complet = pprint(sym.integrate(sys.argv[3]), use_unicode=True)
+            # sympy.doit()
+            # sym.doit(complet)
+            # print(f'#\t{complet}\t#')
+            
+            # pprint(complet.doit())
+            sys.exit(0)
+        elif sys.argv[2] == "--der":
+            reincibilization = "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+            complet = pprint(str(sym.Derivative(sys.argv[3])), use_unicode=True)
+            complet = pprint(sym.Derivative(sys.argv[3]), use_unicode=True)
+            print(f'{reincibilization}')
+            for u in range(0, 1):
+                time.sleep(0.2)
+                
+                complet = pprint(sym.diff(sys.argv[3]), use_unicode=True )
+            # complet.doit()
+            sys.exit(0)
+        elif sys.argv[2] == "--lim":
+            reems = "@##@#@#@#@#@#@#@#@#@#@#@#@@@#@##@##@#@#@#@#@#@"
+            complet = pprint(str(sym.Limit(sys.argv[3])),  use_unicode=True)
+            complet = pprint(sym.Limit(sys.argv[3]),  use_unicode=False)
+            
+            print(f'{reems}')
+        
+            for z in range(0, 1 ):
+                time.sleep(0.1)
+                complet = pprint(sym.limit(sys.argv[3]), use_unicode=True)        
+            # complet.doit()
+            sys.exit(0)
+        # elif  sys.argv[2] == "--lim"
+    else:
+
+        name_x=input('integral : ')
+        if str(name_x):
+            complt = pprint(str(sym.Integral(name_x)), use_unicode=True)
+            # print('jerso3')
+            complt = pprint(sym.Integral(name_x), use_unicode=True)
+            print('resolving .....')
+            for a in range(0, 1):
+                complt = pprint(sym.integrate(name_x) , use_unicode=True)
+            # complt.doit()
+                sys.exit(0)
+        # pprint(str(Integral(ln x, x)))
+        # aprint(complt)
+            #pprint(Integral(sqrt(1/x), x), use_unicode=True)
+
+
+# if __name__ == '__main__':
+#    for z in range(0, 1):
+#        time.sleep(0.5)
+#        limitation(sys.argv[1], sys.argv[2], sys.argv[3])
+        # banner()
+        # eleven()
---
+++ solution.py
@@ -0,0 +1,204 @@
+#!/usr/bin/python3
+
+
+# from distutils.log import debug
+# from crypt import methods
+# from unittest import result
+# from crypt import methods
+from crypt import methods
+import flask
+from flask import Flask , render_template
+from flask import request, jsonify
+import sys 
+import math
+import json
+#from onjson.strucs import builder 
+import strucscode
+# from strucscode 
+# import strucs
+import time
+import sympy as sym 
+from sympy import *
+from sympy.abc import *
+from sympy.parsing.sympy_parser import *
+from brainsolver import diferentials
+
+# how fix import text str in parser render template host in code sussesfully
+# again imported code alll seeder 
+import os 
+
+# from numpy import char 
+
+
+
+
+app = Flask(__name__)
+@app.route('/')
+def main():
+    return render_template("firts_loggin.html")
+
+
+
+
+@app.route('/redirect', methods=["GET, POST"])
+def redirect():
+    if request.method == "POST":
+        obster = request.form["Get Fresh"]
+        return render_template("index.html")
+
+
+
+@app.route('/clubsters', methods=["POST"])
+def clubsters():
+    if request.method == "POST":
+        charger = request.form["value_1"]
+        #evaluator = str(request.form['result'])
+        def buildman(operation):
+            
+            mmmaker =  eval(operation)
+            # print(mmmaker)
+            # if str(operation):
+            # buildman(eval("operation"))
+
+                # print(f'result --> {make}')
+        # buildman(charger)
+        # operr = buildman--(charger)
+        if str(charger):
+            make = eval(charger)
+            print(float(make))
+            return render_template("index.html", result=float(make))
+        # return render_template("index.html", )
+    # pass
+@app.route('/strangerman', methods=["POST"])
+def strangerman():
+    if request.method == "POST":
+        kwork = int(request.form["sq_raiz"])
+        if int(kwork):
+            pth = math.sqrt(kwork)          
+            return render_template('index.html', ravengold=pth)
+
+
+
+
+@app.route('/matrix', methods=["POST"])
+
+
+def matrix():
+    pass
+
+
+
+@app.route('/derivate', methods=["POST"])
+def derivate():
+    if request.method == "POST":
+        composer = request.form["ingral_1"]
+        voold =  request.form["verify"]
+
+        aproved = diferentials(composer)
+        with open("fixhtml/output1.txt", 'r') as buildman:
+            
+            for  exm in  buildman:
+            # buildman_cont = buildman.read()
+                dumpyf = exm.read()
+
+@app.route('/limited', methods=["POST"])
+
+def limited():
+    if request.method == "POST":
+        composer = request.form["limited_1"]
+        limon = request.form["xam"]
+        strapp = request.form["fin_xam"]
+        voold =  request.form["limview"]
+
+        aproved = diferentials(composer)
+        with open("fixhtml/output2.txt", 'r') as buildman:
+            
+            # buildman_cont = buildman.read()
+            for  exm in  buildman:
+                dumpyf = exm.read()
+
+
+
+@app.route('/integrals', methods=["POST"])
+def integrals():
+    if request.method == "POST":
+        composer = request.form["ingral_1"]
+        voold =  request.form["verify"]
+
+        aproved = diferentials(composer)
+        with open("fixhtml/output.txt", 'r') as buildman:
+            
+            # buildman_cont = buildman.read()
+            for  exm in  buildman:
+                dumpyf = exm.read()
+                # print(exm)
+            # buildman = open("fixhtml/output.txt", 'r')
+        
+        # dumpyf = buildman.readline()
+        # dumpyf = buildman.readline()
+        # dumpyf = buildman.readline()
+        # comprovating in line in text 
+        # dumpyf.strip()
+        # dumpfy = buildman.read()
+        
+        # for yum in dumpyf:
+             
+        # stabler = pprint(dumpyf[0:])
+        # def brainsolver(example):
+        #     x , y, z = sym.symbols('x, y , z')
+        #     w , t , u =  sym.symbols('w, t, u')
+        #     v, s, a , b = sym.symbols('v , s, a, b')
+
+            # igral = integrate(example)
+            # print(igral)
+        # brainsolver(composer)
+        # restap = jsonify({"solution" : buildman_cont})
+        # print(aproved)
+        # def brainderive(exaout):
+        #     x , y, z = sym.symbols('x y z')
+        #     w , t , u =  sym.symbols('w  t u')
+        #     v, s, a , b = sym.symbols('v s a b')
+        #      init_printing(use_unicode=True)
+        #     igral = diff(exaout)
+            # igral
+        # new_val = brainderive(composer)
+
+        # if int(composer):
+        #     pass
+
+        if  voold == "integral":
+            return render_template('index.html', hoolview=dumpyf)
+            # return render_template('index.html', hoolview=dumpfy)
+        elif voold == "derivate":
+            return render_template('index.html', hoolview=composer)
+       
+        else:
+            return render_template('error.html')
+
+@app.route('/yettramp', methods=['POST'])
+def yettramp():
+
+    stramper = json.loads(structs)
+    print(stramper['build_name'])
+    # pass
+
+
+@app.route('/saveme', methods=["POST"])
+def saveme( assembly , declif ):
+    updat_file = os.path.join('loggin.txt')
+    alice = open(updat_file, 'w')
+    chunck = 0
+    dumping = request.form[assembly]
+
+    for z in alice:
+        chunck += 1
+        z.write('#[chunck] --> dumping %H:%M:%S \n')
+        # print(z)
+
+app.run(debug=True, port=5633 ) 
+if __name__ == "__main__":
+    clubsters()
+    strangerman()
+    main()
+    redirect()
+    # pass
